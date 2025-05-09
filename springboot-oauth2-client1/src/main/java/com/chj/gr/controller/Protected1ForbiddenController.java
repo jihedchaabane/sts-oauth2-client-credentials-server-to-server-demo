a48a5ebc33d4b7d269ed9b872b555276fc136384
jihed.chaabane <jihed.chaabanee@gmail.com>
2025-05-09 16:11:11 +0200

    ..

--- springboot-oauth2-client1/src/main/java/com/chj/gr/controller/Protected1ForbiddenController.java
+++ springboot-oauth2-client1/src/main/java/com/chj/gr/controller/Protected1ForbiddenController.java
@@ -2,7 +2,6 @@ package com.chj.gr.controller;
 
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,7 +14,7 @@ public class Protected1ForbiddenController {
 	 */
 	@GetMapping("/forbidden")
 	@PreAuthorize("hasAuthority('SCOPE_somescope')")
-    public String protectedEndpoint(@RequestHeader("Authorization") String token) {
-        return "This is a protected API. Token received: " + token;
+    public String protectedEndpoint() {
+        return "Hello from SECURED client1 ..";
     }
 }
