9bf646a8ce3bd53e93878dd269551d2a52ab8423
jihed.chaabane <jihed.chaabanee@gmail.com>
2025-05-26 14:33:29 +0200

    remove auth-server submodule

--- sts-spring-boot-authorization-server/src/main/java/com/chj/gr/config/DefaultSecurityConfig.java
+++
@@ -1,7 +0,0 @@
-package com.chj.gr.config;
-
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-
-@EnableWebSecurity
-public class DefaultSecurityConfig {
-}
