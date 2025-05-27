81f33fcbeacc83ed0f50c162cb1785f0d9e0f255
jihed.chaabane <jihed.chaabanee@gmail.com>
2025-05-27 18:00:10 +0200

    resttemplate consumer adding foo client to consume from the gateway

--- sts-oauth2-consumer-resttemplate/src/main/java/com/chj/gr/controller/easy/FOOPublicController.java
+++ sts-oauth2-consumer-resttemplate/src/main/java/com/chj/gr/controller/easy/FOOPublicController.java
@@ -1,9 +1,10 @@
 package com.chj.gr.controller.easy;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.client.RestTemplate;
 
 import com.chj.gr.config.properties.CallerDestinationProperties;
 import com.chj.gr.config.properties.CallerDestinationProperties.DestinationClient;
@@ -13,12 +14,12 @@ import com.chj.gr.enums.EnumResourceServer;
 @RequestMapping("/public/foo")
 public class FOOPublicController {
 
-    private WebClient webClient;
+	@Autowired
+    private RestTemplate restTemplate;
     
     private CallerDestinationProperties callerDestinationProperties;
     
-    public FOOPublicController(WebClient webClient, CallerDestinationProperties callerDestinationProperties) {
-		this.webClient = webClient;
+    public FOOPublicController(CallerDestinationProperties callerDestinationProperties) {
 		this.callerDestinationProperties = callerDestinationProperties;
 	}
     
@@ -26,111 +27,95 @@ public class FOOPublicController {
     public String get() {
     	DestinationClient destinationClient = callerDestinationProperties.getDestinationClient(
     			EnumResourceServer.FOO_REGISTRATION.getKey());
-        String responseJson = this.webClient
-        		.get()
-                .uri(destinationClient.getResourceUri().concat("/ms7/z-springboot-foo-service/get"))
-                .retrieve()
-                .bodyToMono(String.class)
-                .block();
         
-        return responseJson;
+        String response = restTemplate.getForObject(
+        		destinationClient.getResourceUri() + "/ms7/z-springboot-foo-service/get",
+                String.class
+        );
+        return response;
     }
     
     @GetMapping(value = "/ms7/z-springboot-foo-service/foo-properties")
     public String fooProperties() {
     	DestinationClient destinationClient = callerDestinationProperties.getDestinationClient(
     			EnumResourceServer.FOO_REGISTRATION.getKey());
-        String responseJson = this.webClient
-        		.get()
-                .uri(destinationClient.getResourceUri().concat("/ms7/z-springboot-foo-service/foo-properties"))
-                .retrieve()
-                .bodyToMono(String.class)
-                .block();
         
-        return responseJson;
+        String response = restTemplate.getForObject(
+        		destinationClient.getResourceUri() + "/ms7/z-springboot-foo-service/foo-properties",
+                String.class
+        );
+        return response;
     }
     
     @GetMapping(value = "/ms7/z-springboot-foo-service/bar")
     public String bar() {
     	DestinationClient destinationClient = callerDestinationProperties.getDestinationClient(
     			EnumResourceServer.FOO_REGISTRATION.getKey());
-        String responseJson = this.webClient
-        		.get()
-                .uri(destinationClient.getResourceUri().concat("/ms7/z-springboot-foo-service/bar"))
-                .retrieve()
-                .bodyToMono(String.class)
-                .block();
         
-        return responseJson;
+    	String response = restTemplate.getForObject(
+        		destinationClient.getResourceUri() + "/ms7/z-springboot-foo-service/bar",
+                String.class
+        );
+        return response;
     }
     
     @GetMapping(value = "/ms7/z-springboot-foo-service/bar-properties")
     public String barProperties() {
     	DestinationClient destinationClient = callerDestinationProperties.getDestinationClient(
     			EnumResourceServer.FOO_REGISTRATION.getKey());
-        String responseJson = this.webClient
-        		.get()
-                .uri(destinationClient.getResourceUri().concat("/ms7/z-springboot-foo-service/bar-properties"))
-                .retrieve()
-                .bodyToMono(String.class)
-                .block();
         
-        return responseJson;
+        String response = restTemplate.getForObject(
+        		destinationClient.getResourceUri() + "/ms7/z-springboot-foo-service/bar-properties",
+                String.class
+        );
+        return response;
     }
     
     @GetMapping(value = "/ms7/z-springboot-foo-service/hello")
     public String hello() {
     	DestinationClient destinationClient = callerDestinationProperties.getDestinationClient(
     			EnumResourceServer.FOO_REGISTRATION.getKey());
-        String responseJson = this.webClient
-        		.get()
-                .uri(destinationClient.getResourceUri().concat("/ms7/z-springboot-foo-service/hello"))
-                .retrieve()
-                .bodyToMono(String.class)
-                .block();
         
-        return responseJson;
+        String response = restTemplate.getForObject(
+        		destinationClient.getResourceUri() + "/ms7/z-springboot-foo-service/hello",
+                String.class
+        );
+        return response;
     }
     
     @GetMapping(value = "/ms7/z-springboot-foo-service/helloworld")
     public String helloworld() {
     	DestinationClient destinationClient = callerDestinationProperties.getDestinationClient(
     			EnumResourceServer.FOO_REGISTRATION.getKey());
-        String responseJson = this.webClient
-        		.get()
-                .uri(destinationClient.getResourceUri().concat("/ms7/z-springboot-foo-service/helloworld"))
-                .retrieve()
-                .bodyToMono(String.class)
-                .block();
         
-        return responseJson;
+        String response = restTemplate.getForObject(
+        		destinationClient.getResourceUri() + "/ms7/z-springboot-foo-service/helloworld",
+                String.class
+        );
+        return response;
     }
     
     @GetMapping(value = "/ms7/z-springboot-foo-service/helloworld/hello-property")
     public String helloProperty() {
     	DestinationClient destinationClient = callerDestinationProperties.getDestinationClient(
     			EnumResourceServer.FOO_REGISTRATION.getKey());
-        String responseJson = this.webClient
-        		.get()
-                .uri(destinationClient.getResourceUri().concat("/ms7/z-springboot-foo-service/helloworld/hello-property"))
-                .retrieve()
-                .bodyToMono(String.class)
-                .block();
         
-        return responseJson;
+        String response = restTemplate.getForObject(
+        		destinationClient.getResourceUri() + "/ms7/z-springboot-foo-service/helloworld/hello-property",
+                String.class
+        );
+        return response;
     }
     
     @GetMapping(value = "/ms7/z-springboot-foo-service/helloworld/display-properties")
     public String displayProperties() {
     	DestinationClient destinationClient = callerDestinationProperties.getDestinationClient(
     			EnumResourceServer.FOO_REGISTRATION.getKey());
-        String responseJson = this.webClient
-        		.get()
-                .uri(destinationClient.getResourceUri().concat("/ms7/z-springboot-foo-service/helloworld/display-properties"))
-                .retrieve()
-                .bodyToMono(String.class)
-                .block();
         
-        return responseJson;
+        String response = restTemplate.getForObject(
+        		destinationClient.getResourceUri() + "/ms7/z-springboot-foo-service/helloworld/display-properties",
+                String.class
+        );
+        return response;
     }
 }
--- sts-oauth2-consumer-resttemplate/src/main/java/com/chj/gr/enums/EnumResourceServer.java
+++ sts-oauth2-consumer-resttemplate/src/main/java/com/chj/gr/enums/EnumResourceServer.java
@@ -6,7 +6,8 @@ package com.chj.gr.enums;
 public enum EnumResourceServer {
 	STS_DEFAULT_REGISTRATION(						"client0",  "sts-default-registration"),
 	STS_OAUTH2_CLIENT1_RESOURCE_SERVER_REGISTRATION("client1",  "sts-oauth2-client1-resource-server-registration"),
-	STS_OAUTH2_CLIENT2_RESOURCE_SERVER_REGISTRATION("client2",  "sts-oauth2-client2-resource-server-registration");
+	STS_OAUTH2_CLIENT2_RESOURCE_SERVER_REGISTRATION("client2",  "sts-oauth2-client2-resource-server-registration"),
+	FOO_REGISTRATION("client3", "sts-foo-registration");
 
     private final String key;
     private final String registrationId;
